762f890124a54acd0748a24cbe7f975a6633c928
Yug <03yugagarwal@gmail.com>
2023-06-23 22:19:24 +0530

    student info

--- client/src/components/student/StudentInfo.js
+++ client/src/components/student/StudentInfo.js
@@ -1,26 +1,43 @@
 import React, { useEffect, useState } from "react";
+import Button from "../UI/Button";
 
 const StudentInfo = () => {
   const [details, setDetails] = useState("");
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
+  let token = ""
+
+  const handleMyInfo = () => {
+
+    token = localStorage.getItem("token");
 
     fetch("http://localhost:8000/api/v1/student/me", {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setDetails(data);
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
       })
-      .catch((error) => console.error(error));
-  }, []);
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setDetails(data);
+        })
+        .catch((error) => console.error(error));
+  }
 
-  return <></>;
+  return <div>
+    <h1>My Info</h1>
+    <h4>General Info</h4>
+    <p>Name: {details.data.name}</p>
+    <p>RegNo: {details.data.regNo}</p>
+    <p>Block: {details.data.block}</p>
+    <p>RoomNo: {details.data.roomNo}</p>
+    <p>Mess: {details.data.messType}</p>
+
+    <Button onClick={handleMyInfo}>My Info</Button>
+
+    <Button>Warden Info</Button>
+
+  </div>;
 };
 
 export default StudentInfo;
